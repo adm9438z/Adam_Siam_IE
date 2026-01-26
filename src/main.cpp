4b0741d0676754677cedef948a548f7c5905cb6d
adm9438z <adamahssayene2@gmail.com>
2026-01-26 11:56:56 +0100

    TEST_TP1

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,29 @@ void setup() {
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
-  lcd.printf("Trieur de balles");
+  lcd.setRGB(112,0,0);
+
+  pinMode(0,INPUT_PULLUP);
+  pinMode(2,INPUT_PULLUP);
+  pinMode(12,INPUT_PULLUP);
 
 }
 
 void loop() {
+  int bp0 = digitalRead(0);
+  int bp1 = digitalRead(2);
+  int bp2 = digitalRead(12);
+  char pot = analogRead(33);
 
+  Serial.printf("bp0=%d ",bp0);
+  Serial.printf("bp1=%d \n",bp1);
+  Serial.printf("bp2=%d ",bp2);
+  Serial.printf("pot = %d",pot);
+
+  lcd.printf("bp0 = %d ",bp0);
+  lcd.printf("bp1 = %d \n",bp1);
+  lcd.printf("bp2 = %d ",bp2);
+  lcd.printf("pot = %d",pot); 
+  
+return;
 }
