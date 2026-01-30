4730c558e3b66270c0b379eba4678220f3158480
Adam94380* <adam.ahssayene2@gmail.com>
2026-01-30 10:43:45 +0100

    Encodeur

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,36 @@ void setup() {
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
-  lcd.setRGB(112,0,0);
+  lcd.setRGB(85,120,120);
 
   pinMode(0,INPUT_PULLUP);
   pinMode(2,INPUT_PULLUP);
   pinMode(12,INPUT_PULLUP);
 
+  pinMode(26,OUTPUT);
+
   ledcAttachPin(27,0);
-  ledcSetup(0,25000,8);
+  ledcSetup(0,25000,11);
 }
 
 void loop() {
   int bp0 = digitalRead(0);
   int bp1 = digitalRead(2);
   int bp2 = digitalRead(12);
-  char pot = analogRead(33);
+  int pot = analogRead(33);
 
   Serial.printf("bp0=%d ",bp0);
   Serial.printf("bp1=%d \n",bp1);
   Serial.printf("bp2=%d ",bp2);
   Serial.printf("pot = %d",pot);
 
-  lcd.printf("bp0 = %d ",bp0);
-  lcd.printf("bp1 = %d \n",bp1);
-  lcd.printf("bp2 = %d ",bp2);
-  lcd.printf("pot = %d",pot); 
-  
-ledcWrite(0,pot/2);
+  //lcd.printf("bp0 = %d ",bp0);
+  //lcd.printf("bp1 = %d \n",bp1);
+  //lcd.printf("bp2 = %d ",bp2);
+  //lcd.printf("pot = %d",pot); 
 
+ledcWrite(0,pot/2);
+digitalWrite(26,bp0);
 
 return;
 }
